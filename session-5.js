e241b4f64b8d21441c4bf4f9945d2b49f4c18153
GautamMahato <gautammahato@ipangram.com>
2023-06-08 18:49:31 +0530

    loop

--- session-5.js
+++ session-5.js
@@ -223,3 +223,284 @@ console.log(newNum)
 [ 3, 14, 5, 6, 7 ]
 
 */
+
+//How to access Array from the loop
+
+let nmbr = [1, 2, 3, 4, 5, 6]
+for (let i = 0; i<nmbr.length;i++){
+  console.log(nmbr[i])
+}
+
+/*output
+1
+2
+3
+4
+5
+*/
+
+
+
+
+// Task - (nmbr[i]) *  (nmbr[i])            it helps more when we make react apps
+let nmb = [1, 2, 3, 4, 5, 6]
+nmb.forEach((element)=>{
+  console.log(element * element)
+})
+/*output-
+
+1
+4
+9
+16
+25
+36
+
+*/
+
+
+
+// Array from loop -  it is usd to create a array from any other object
+let name = "Rishigyanu"
+let arr = Array.from(name)
+console.log(arr)
+
+/*output-
+[
+  'R', 'i', 's', 'h',
+  'i', 'g', 'y', 'a',
+  'n', 'u'
+]
+*/
+
+//for...of loop - can be use to get the value from any array.
+let n = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+for(let i of n){
+  console.log(i)
+}
+
+/*output
+551
+22
+3
+14
+5
+6
+7
+8
+229
+*/
+
+//for...in loop - can be use to get the key from any array.
+let o = [1, 2, 3, 4, 5, 6]
+for (let i in o){
+  console.log(i)
+}
+
+/*output
+0
+1
+2
+3
+4
+5
+*/
+
+//forEach loop   - it is use to sqaure the index in an boject 
+let p = [1, 2, 3, 4, 5, 6]
+p.forEach((Element)=>{
+  console.log(Element * Element)
+})
+
+/*output
+1
+4
+9
+16
+25
+36
+*/
+
+
+//>>>>>>>>>>>> Higher Order Method <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//>>>>>>. map, filter, Reduce <<<<<<<<<<<<<<<<<<<< 
+// it reduce the time consuption.
+
+
+// Array map() Method  - it use to create a new array by programing some operation on each array elements
+let ar = [1, 2, 3, 4, 5]
+let s = ar.map((value)=>{
+  console.log(value)
+ 
+})
+console.log(s)
+
+/*output
+1
+2
+3
+4
+5
+*/
+
+
+
+//How to print value, index, array
+let ar = [1, 2, 3, 4, 5]
+let s = ar.map((value, index, array)=>{
+  console.log(value, index, array)
+ 
+})
+console.log(s)
+
+/*output
+1 0 [ 1, 2, 3, 4, 5 ]
+2 1 [ 1, 2, 3, 4, 5 ]
+3 2 [ 1, 2, 3, 4, 5 ]
+4 3 [ 1, 2, 3, 4, 5 ]
+5 4 [ 1, 2, 3, 4, 5 ]
+*/
+
+
+
+//How to get + 1 value of existing array
+let ar = [1, 2, 3, 4, 5]
+let s = ar.map((value, index, array)=>{
+  console.log(value, index, array)
+  return value + 1
+ 
+})
+console.log(s)
+/*output
+1 0 [ 1, 2, 3, 4, 5 ]
+2 1 [ 1, 2, 3, 4, 5 ]
+3 2 [ 1, 2, 3, 4, 5 ]
+4 3 [ 1, 2, 3, 4, 5 ]
+5 4 [ 1, 2, 3, 4, 5 ]
+[ 2, 3, 4, 5, 6 ]                this is new Array with +1
+*/
+
+
+
+//>>>>>>>>>>>> Filter <<<<<<<<<<<<<<<<<<<
+// How to filter the value of less than 10
+let arr1 = [0, 1, 2, 3, 4, 22, 11, 33, 44]        // filter method can't modified the original Array
+let a2 = arr1.filter((a)=>{
+  return a<10
+})
+console.log(a2)
+
+/*output
+[ 0, 1, 2, 3, 4 ]
+*/
+
+
+//>>>>>>>>>>>>> Reduce <<<<<<<<<<<<<<<<<<
+let arr3 = [1, 2, 3, 4]
+let newArr3 = arr3.reduce((h1, h2)=>{
+  return h1 + h2
+})
+console.log(newArr3)
+
+/*output                             how to operate it -  1+2 =3
+                                                          3+3 =6
+10                                                        6+4 =10    ans- 10
+
+*/
+
+
+
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+   >>>>>>>>>>>>>>>>>>>>>>>>  Chapter-5 Practice Set <<<<<<<<<<<<<<<<<<<<<<<<<<
+   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+
+/* Q-1 Create an Array of number and take input from the users to add number to this array */
+
+let arr = [1, 2, 3, 4, 83]
+let a = prompt("Enter the Number")
+a = Number.parseInt(a)
+arr.push(a)
+console.log(arr)
+
+/*output
+Enter the Number> 45
+[ 1, 2, 3, 4, 83, 45 ]
+*/
+
+
+/* Q-2 Keep adding number to the array in first question until o is added to the array*/
+let arr = [1, 2, 3, 4, 83]
+let a;
+
+do{
+  a = prompt("Enter the Number")  
+  a = Number.parseInt(a)
+  arr.push(a)
+}while (a != 0);
+console.log(arr)
+/*output
+Enter the Number> 1
+Enter the Number> 2
+Enter the Number> 3
+Enter the Number> 4
+Enter the Number> 5
+Enter the Number> 6
+Enter the Number> 0
+[
+  1, 2, 3, 4, 83,
+  1, 2, 3, 4,  5,
+  6, 0
+]
+*/
+
+/* Q-3 Filter for the number divisible by 10 from a given array */
+let y = [1, 2, 3, 4, 80, 20, 50, 60]
+let z = y.filter((x)=>{
+  return x%10 == 0
+})
+console.log(z)
+
+//output -  [ 80, 20, 50, 60 ]
+
+
+
+
+
+/* Q-4  Create an array of square of given number */
+let y = [1, 2, 3, 4, 80, 20, 50, 60]
+let z = y.map((x)=>{
+  return x*x
+})
+console.log(z)
+/*output
+[
+     1,    4,   9,
+    16, 6400, 400,
+  2500, 3600
+]
+*/
+
+
+
+
+/*   Q-5 Use Reduce to calculate factorial of a given number from an array of first n 
+         natural number ( n being the number whole factorails need to be calculated       
+*/
+let y = [1, 2, 3, 4]
+let z = y.reduce((x1, x2)=>{
+  return x1 * x2
+})
+console.log(z)
+
+/* output-  24   
+
+how to calculate it 
+1 * 2 = 2
+2 * 3 = 6
+6 * 4 = 24
+
+
+ans - 24
+*/
